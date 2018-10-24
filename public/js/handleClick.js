3bef2d125db1209cf41031fdb396285b6191fa50
Ahisnawi <ayman.hisnawi@axelspringer.de>
2018-10-24 17:05:31 +0200

    submit-button

---
+++ public/js/handleClick.js
@@ -0,0 +1,24 @@
+/*function handleClick() {
+document.getElementById("vehicle1").addEventListener("click", handleClick);
+function handleClick(){
+            if (vehicle1.checked == true){
+            submit.disable = false;    
+            } else {
+            submit.disable = true;    
+            }
+            }
+            
+
+
+}
+<script language="JavsScript">document.getElementById("vehicle1").addEventListener("click", handleClick());
+            function handleClick(){
+            if (vehicle1.checked === true){
+            submit.disable = false;    
+            } else {
+            submit.disable = true;    
+            }
+            }
+            </script>    
+
+*/
